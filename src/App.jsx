79462a4fc7a0a0ca2a23cb197d5dd8f3958c313c
Ruksana <ruksana01official@gmail.com>
2023-09-18 15:34:51 +0600

    18-9-2023(3:32)Processing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import './App.css'
+import LineChart from './components/LineChart/LineChart'
+import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
+import PriceOptions from './components/PriceOptions/PriceOptions'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+
 function App() {
+
   return (
     <>
-      <h1 className='text-7xl'>Vite + React</h1>
+      <NavBar></NavBar>
+      {/* <DaisyNav></DaisyNav> */}
+      <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
+      <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
